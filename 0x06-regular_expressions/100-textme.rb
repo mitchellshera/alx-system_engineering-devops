397985d6b6ac771631fc3e0b5cd2cedbd2ce5bd8
mitchellshera <mitchellwachera@gmail.com>
2023-07-04 16:14:01 +0300

    done

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,13 +1,3 @@
 #!/usr/bin/env ruby
 
-log_file = 'text_document.log'  # Path to the log file
-
-File.foreach(log_file) do |line|
-  match_data = line.match(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
-  if match_data
-    sender = match_data[1]
-    receiver = match_data[2]
-    flags = match_data[3]
-    puts "#{sender},#{receiver},#{flags}"
-  end
-end
+puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
