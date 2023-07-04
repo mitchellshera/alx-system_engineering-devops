31f3711e4205470dd86a0ab127d126343cf69a7d
mitchellshera <mitchellwachera@gmail.com>
2023-07-04 14:52:34 +0300

    task 0

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+input = ARGV[0]  # Get the argument passed to the script
+
+if input =~ /School/
+  puts "School"
+else
+  puts ""  # Print an empty string if there is no match
+end
