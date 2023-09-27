45b8db93deed137b7f858c30587d0c3a1cdf81ac
mitchellshera <mitchellwachera@gmail.com>
2023-09-27 12:43:13 +0300

    dict

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,52 +5,21 @@ import json
 import requests
 import sys
 
+
 if __name__ == "__main__":
-    def fetch_all_employee_data():
-        url = "https://jsonplaceholder.typicode.com/users"
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
 
-        try:
-            # Fetch all user data
-            user_response = requests.get(url)
-            user_data = user_response.json()
+    # Fetch user data
+    user_response = requests.get(url + "users/{}".format(user_id))
+    users= user_response.json()
 
-            all_employee_data = {}
-
-            for user in user_data:
-                user_id = user["id"]
-                username = user["username"]
-
-                # Fetch TODO list data for each user
-                todos_response = requests.get(url + f"/{user_id}/todos")
-                todos = todos_response.json()
-
-                # Prepare data for the user
-                user_tasks = []
-                for task in todos:
-                    user_tasks.append({
-                        "username": username,
-                        "task": task["title"],
-                        "completed": task["completed"]
-                    })
-
-                all_employee_data[user_id] = user_tasks
-
-            return all_employee_data
-
-        except requests.exceptions.RequestException as e:
-            print(f"Error: {e}")
-            return None
-
-    def export_to_json(all_employee_data):
-        json_data = all_employee_data
-        json_filename = "todo_all_employees.json"
-
-        with open(json_filename, "w") as jsonfile:
-            json.dump(json_data, jsonfile, indent=4)
-
-        print(f"JSON data exported to {json_filename}.")
-
-    all_employee_data = fetch_all_employee_data()
-
-    if all_employee_data:
-        export_to_json(all_employee_data)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
