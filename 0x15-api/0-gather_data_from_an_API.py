dd7b03640e7702c31ab01fba2e8d680f24e2d435
mitchellshera <mitchellwachera@gmail.com>
2023-09-27 09:09:11 +0300

    update task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,38 @@
 #!/usr/bin/python3
-'''Returns information about TODO list progress by employee ID.'''
+"""Returns to-do list information for a given employee ID."""
 import requests
-from sys import argv
+import sys
+
+def get_employee_todo_progress(employee_id):
+    # Define the API endpoints
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+
+    try:
+        # Fetch user data
+        user_response = requests.get(user_url)
+        user_data = user_response.json()
+
+        # Fetch TODO list data
+        todo_response = requests.get(todo_url)
+        todo_data = todo_response.json()
+
+        # Filter completed tasks
+        completed_tasks = [task for task in todo_data if task['completed']]
+        total_tasks = len(todo_data)
+
+        # Display employee's progress
+        print(f"Employee {user_data['name']} is done with tasks ({len(completed_tasks)}/{total_tasks}):")
+        
+        for task in completed_tasks:
+            print(f"\t{task['title']}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
 
 if __name__ == '__main__':
-    todos_url = 'https://jsonplaceholder.typicode.com/todos'
-    users_url = 'https://jsonplaceholder.typicode.com/users'
-    
-    todos_response = requests.get(todos_url)
-    users_response = requests.get(users_url)
-    
-    if todos_response.status_code != 200 or users_response.status_code != 200:
-        print('Failed to retrieve data from the API.')
-        exit(1)
-    
-    todos_data = todos_response.json()
-    users_data = users_response.json()
-
-    employee_id = int(argv[1])
-    user = next((u for u in users_data if u['id'] == employee_id), None)
-    
-    if user is None:
-        print(f'Employee with ID {employee_id} not found.')
-        exit(1)
-    
-    tasks = [task for task in todos_data if task['userId'] == employee_id]
-    completed_tasks = [task for task in tasks if task['completed']]
-    
-    print(f"Employee {user['name']} is done with tasks({len(completed_tasks)}/{len(tasks)}):")
-    for task in completed_tasks:
-        print(f"\t{task['title']}")
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+    else:
+        employee_id = int(sys.argv[1])
+        get_employee_todo_progress(employee_id)
