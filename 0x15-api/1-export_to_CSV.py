6eccca235e9c0918f26832a28ea2c2eb857ce80e
mitchellshera <mitchellwachera@gmail.com>
2023-09-27 12:21:54 +0300

    csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Returns to-do list information for a given employee ID and exports it to CSV format."""
 
-import csv
+import json
 import requests
 import sys
 
-def get_employee_todo_progress(user_id):
+if __name__ == "__main__":
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
 
     # Fetch user data
@@ -17,30 +18,17 @@ def get_employee_todo_progress(user_id):
     todos_response = requests.get(url + "todos", params={"userId": user_id})
     todos = todos_response.json()
 
-    # Filter completed tasks
-    completed_tasks = [task for task in todos if task.get("completed")]
+    # Prepare and export data to JSON
+    json_data = {
+        user_id: [
+            {
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": username
+            }
+            for task in todos
+        ]
+    }
 
-    # Display TODO list progress
-    print("Employee {} is done with tasks({}/{}):".format(
-        user_data.get('name'), len(completed_tasks), len(todos)))
-    
-    for task in completed_tasks:
-        print("\t {}".format(task.get('title')))
-
-    # Export to CSV
-    csv_filename = "{}.csv".format(user_id)
-    with open(csv_filename, "w", newline="") as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        
-        for task in completed_tasks:
-            csv_writer.writerow([user_id, username, task.get("completed"), task.get("title")])
-    
-    print(f"CSV data exported to {csv_filename}.")
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 1-export_to_CSV.py <user_id>")
-    else:
-        user_id = sys.argv[1]
-        get_employee_todo_progress(user_id)
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump(json_data, jsonfile)
