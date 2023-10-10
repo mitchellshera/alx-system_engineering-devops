69f5a6dede16eee6c9e54698d33347070845b804
mitchellshera <mitchellwachera@gmail.com>
2023-10-10 14:40:56 +0300

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -15,7 +15,8 @@ def top_ten(subreddit):
     }
 
     try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        response = requests.get(url, headers=headers,
+                                params=params, allow_redirects=False)
 
         if response.status_code == 200:
             data = response.json()
