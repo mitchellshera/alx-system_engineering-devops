6519d30b44f9249c570e45d2f0a0261043dbade9
mitchellshera <mitchellwachera@gmail.com>
2023-10-10 14:18:49 +0300

    reddit api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,19 +2,20 @@
 """querying a redditapi"""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         subreddit_data = response.json().get("data")
-        
+
         if subreddit_data and "subscribers" in subreddit_data:
             return subreddit_data["subscribers"]
-    
+
     return 0
