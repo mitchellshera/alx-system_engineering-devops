ff3088294f8adee4dd24fdbb54369c3568069030
mitchellshera <mitchellwachera@gmail.com>
2023-10-17 22:01:09 +0300

    ulimit nginx

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Update the ULIMIT parameter in the nginx configuration file and restart Nginx
+exec { 'nginx_ulimit':
+  command => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx && sudo service nginx restart',
+  path    => ['/usr/local/bin', '/bin'],
+  refreshonly => true,
+}
