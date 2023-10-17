273be3ebd7370a3fabe798ab6660558f82127d0f
mitchellshera <mitchellwachera@gmail.com>
2023-10-17 20:34:55 +0300

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix a 500 error when a GET method is requested to Apache web server for wordpress
 
-exec {"fix-wordpress":
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
-  path    => '/usr/local/bin/:/bin/'
+exec {'fix-wordpress':
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path  => '/usr/local/bin/:/bin/'
 }
