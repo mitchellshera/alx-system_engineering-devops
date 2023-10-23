5b1b3b1b45c858d0b4041eb5b0fe765006cd93d5
mitchellshera <mitchellwachera@gmail.com>
2023-10-23 16:16:58 +0300

    01

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,11 @@
 # Update the ULIMIT parameter in the nginx configuration file and restart Nginx
-exec { 'nginx_ulimit':
-  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx && sudo service nginx restart',
-  path   => ['/usr/local/bin', '/bin'],
-  refreshonly => true,
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,11 @@
 # Update nofile limits in the /etc/security/limits.conf file
-exec { 'update_nofile_limits':
-  command  => 'sudo sed -i "s/nofile 5/nofile 50000/; s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+exec {'update_nofile_limits':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['update_nofile_limits-2'],
+}
+
+exec {'update_nofile_limits-2':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
 }
